9a9beab9e3862b43783ea08517022768ae78966f
KunWoo Shin <spacedustw@gmail.com>
2023-12-09 00:17:49 +0900

    Add RedisConstants

---
+++ Statistics-Backend/src/main/java/com/statistics/constans/RedisConstants.java
@@ -0,0 +1,9 @@
+package com.statistics.constans;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class RedisConstants {
+    public static final String REDIS_KEY_CAMERA_STATS = "camera:stats:";
+    public static final String REDIS_KEY_TRIPWIRE_STATS = "camera:tripwire:stats:";
+}
