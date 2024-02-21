1fe4a09444bae2c12588341456c57ec880e7ea5c
Kunwoo Shin <recyclefunction@gmail.com>
2024-02-21 17:53:21 +0900

    Date Util - timestamp 변환 함수 추가

--- Statistics/src/main/java/statistics/util/DateUtil.java
+++ Statistics/src/main/java/statistics/util/DateUtil.java
@@ -1,6 +1,12 @@
 package statistics.util;
 
+import org.springframework.util.StringUtils;
+
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,5 +17,23 @@ public class DateUtil {
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
         return format.format(calendar.getTime());
+    }
+
+    public static String timestampToDate(final long timestamp, final String zone) {
+        LocalDateTime localDateTime = null;
+
+        if (!StringUtils.hasText(zone)) {
+            localDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp),
+                    ZoneId.systemDefault());
+
+        } else {
+            localDateTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneId.of(zone));
+        }
+
+        // LocalDateTime 객체를 원하는 포맷으로 문자열로 변환
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String formattedDate = localDateTime.format(formatter);
+
+        return formattedDate;
     }
 }
