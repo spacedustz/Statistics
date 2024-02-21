3d03fae26c8592822ce8d603a8dbb54136ff3b2c
Kunwoo Shin <recyclefunction@gmail.com>
2024-02-21 17:52:39 +0900

    Redis Constants 추가

---
+++ Statistics/src/main/java/statistics/constants/RedisConstants.java
@@ -0,0 +1,6 @@
+package statistics.constants;
+
+public class RedisConstants {
+    public static final String INSTANCE = "instance:";
+    public static final String INSTANCE_COUNT = "instance:stats:";
+}
