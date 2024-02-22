571939e28636f4bc69cffc8de80b543b0d38af61
owen <floralexium@gmail.com>
2024-02-22 17:06:05 +0900

    Redis Constants 필드명 변경

--- Statistics/src/main/java/statistics/constants/RedisConstants.java
+++ Statistics/src/main/java/statistics/constants/RedisConstants.java
@@ -2,7 +2,7 @@ package statistics.constants;
 
 public class RedisConstants {
     public static final String INSTANCE = "instance:";
-    public static final String INSTANCE_COUNT = "instance:count:";
-    public static final String INSTANCE_CURRENT_ALARM_LEVEL = "instance:current:alarm:level";
-    public static final String INSTANCE_LATEST_ALARM_LEVEL = "instance:latest:alarm:level";
+    public static final String INSTANCE_COUNT = "count:";
+    public static final String INSTANCE_CURRENT_ALARM_LEVEL = "current:alarm:level:";
+    public static final String INSTANCE_LATEST_ALARM_LEVEL = "latest:alarm:timestamp:";
 }
