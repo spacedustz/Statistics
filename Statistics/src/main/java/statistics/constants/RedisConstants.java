7c5aedb255329898620263cae4b381178d5b7d9e
owen <wavefunctionc@gmail.com>
2024-02-22 15:47:13 +0900

    Redis Constants 필드 추가

--- Statistics/src/main/java/statistics/constants/RedisConstants.java
+++ Statistics/src/main/java/statistics/constants/RedisConstants.java
@@ -3,4 +3,6 @@ package statistics.constants;
 public class RedisConstants {
     public static final String INSTANCE = "instance:";
     public static final String INSTANCE_COUNT = "instance:count:";
+    public static final String INSTANCE_CURRENT_ALARM_LEVEL = "instance:current:alarm:level";
+    public static final String INSTANCE_LATEST_ALARM_LEVEL = "instance:latest:alarm:level";
 }
