b45b395306d75a80aa520055a5d8cc6de3600fcd
spacedustz <spacedustw@gmail.com>
2024-02-22 15:47:24 +0900

    Application Constants 필드 추가

--- Statistics/src/main/java/statistics/constants/ApplicationConstants.java
+++ Statistics/src/main/java/statistics/constants/ApplicationConstants.java
@@ -2,4 +2,14 @@ package statistics.constants;
 
 public class ApplicationConstants {
     public static final String SEOUL_TIMEZONE = "Asia/Seoul";
+
+    public static final String ALARM_LEVEL_1 = "ALM0000001";   // 알람상태(관심)
+
+    public static final String ALARM_LEVEL_2 = "ALM0000002";   // 알람상태(주의)
+
+    public static final String ALARM_LEVEL_3 = "ALM0000003";   // 알람상태(경계)
+
+    public static final String ALARM_LEVEL_4 = "ALM0000004";   // 알람상태(심각)
+
+    public static final Integer CHECK_ALARM_INTERVAL = 60;
 }
