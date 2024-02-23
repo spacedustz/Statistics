d07ad93ba78e78b9a25149a5668a7a0174ad1c90
spacedustz <spacedustw@gmail.com>
2024-02-23 15:31:52 +0900

    15초 통계 처리 함수 루가

--- Statistics/src/main/java/statistics/service/StatisticsService.java
+++ Statistics/src/main/java/statistics/service/StatisticsService.java
@@ -3,12 +3,20 @@ package statistics.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import statistics.entity.Svc15SecPk;
 import statistics.entity.Svc15SecStat;
 import statistics.repository.Svc15SecStatRepository;
+import statistics.service.redis.RedisService;
+import statistics.thread.StatisticsThread;
+import statistics.util.DateUtil;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Service
@@ -16,6 +24,8 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class StatisticsService {
     private final Svc15SecStatRepository svc15SecStatRepository;
+    private final RedisService redisService;
+    private final TaskExecutor executor;
 
     public void save15SecAvgToDB(final String yyyymmdd,
                                  final String hhmiss,
@@ -40,6 +50,30 @@ public class StatisticsService {
             svc15SecStatRepository.save(stats);
         } catch (Exception e) {
             log.error("15초 통계 저장 실패 - {}", e.getMessage());
+        }
+    }
+
+    @Scheduled(cron = "3,18,33,48 * * * * *")
+    public void calculate15SecStats() {
+        List<String> keyList = new ArrayList<>();
+        String currentTime = DateUtil.getTime();
+        Set<String> statsKeys = redisService.getAllStatsKeys();
+
+        for (String key : statsKeys) {
+            keyList.add(key);
+
+            if (keyList.size() >= 15) {
+                List<String> keyListCopy = new ArrayList<>(keyList);
+                StatisticsThread thread = new StatisticsThread(keyListCopy, redisService, this);
+
+                executor.execute(thread);
+                keyList.clear();
+            }
+        }
+
+        if (!keyList.isEmpty()) {
+            StatisticsThread thread = new StatisticsThread(keyList, redisService, this);
+            executor.execute(thread);
         }
     }
 }
