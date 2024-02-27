baaa8737f12bbe111545042510f96b8c2e040b35
Kunwoo Shin <recyclefunction@gmail.com>
2024-02-27 16:22:50 +0900

    통계 Repository 전부 생성

---
+++ Statistics/src/main/java/statistics/repository/Svc1MinStatRepository.java
@@ -0,0 +1,8 @@
+package statistics.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import statistics.entity.Svc1MinPk;
+import statistics.entity.Svc1MinStat;
+
+public interface Svc1MinStatRepository extends JpaRepository<Svc1MinStat, Svc1MinPk> {
+}
