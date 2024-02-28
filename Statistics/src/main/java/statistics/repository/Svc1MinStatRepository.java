3c1a97060d68d0f077756c2e27a282455b156692
spacedustz <spacedustw@gmail.com>
2024-02-28 18:00:35 +0900

    1분 통계 Repository, Native Query cnrk

--- Statistics/src/main/java/statistics/repository/Svc1MinStatRepository.java
+++ Statistics/src/main/java/statistics/repository/Svc1MinStatRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import statistics.entity.Svc1MinPk;
 import statistics.entity.Svc1MinStat;
 
-public interface Svc1MinStatRepository extends JpaRepository<Svc1MinStat, Svc1MinPk> {
+public interface Svc1MinStatRepository extends JpaRepository<Svc1MinStat, Svc1MinPk>, Svc1MinStatRepositoryCustom {
 }
